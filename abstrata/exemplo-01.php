cbc8557112c9982c70896f254ac7808639591ae9
Daniel <dfcastro@outlook.com.br>
2024-08-04 20:26:09 -0300

    abstrata

---
+++ abstrata/exemplo-01.php
@@ -0,0 +1,42 @@
+<?php
+
+    interface Veiculo{
+
+        public function acelerar($velocidade);
+        public function frenar($velocidade);
+        public function trocarMarcha($marcha);
+
+                
+    }
+
+   abstract class Automovel implements Veiculo{
+
+        public function acelerar($velocidade)
+        {
+            echo "o veiculo acelerou até" . $velocidade." km/h";
+        }
+        public function frenar($velocidade)
+        {
+            echo "o veiculo frenou até " . $velocidade." km/h";
+        }
+        public function trocarMarcha($marcha)
+        {
+            echo "trocou de marcha ". $marcha;
+        }
+
+    }
+
+    class DelRey extends Automovel{
+
+        public function empurar()
+        {
+
+        }
+
+     }
+
+     $carro = new Automovel();
+
+     $carro->acelerar(200);
+
+?>
