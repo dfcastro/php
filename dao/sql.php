3994a5785958024074d12102c92a02bec167a835
Daniel <dfcastro@outlook.com.br>
2024-10-02 15:00:12 -0300

    primeiro dao

--- dao/sql.php
+++ dao/sql.php
@@ -1,37 +1,49 @@
 <?php
 
-    class Sql extends PDO{
+class Sql extends PDO
+{
 
-        private $conn;
+    private $conn;
 
 
-        public function __construct(){
+    public function __construct()
+    {
 
-            $this->conn = new PDO("mysql:dbname=dbphp7;host=localhost","root","");
-        }
+        $this->conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
+    }
 
-        private function setParams($statment, $parameters = array()){
+    private function setParams($statment, $parameters = array())
+    {
 
-            foreach($parameters as $key => $value){
+        foreach ($parameters as $key => $value) {
 
-               $this->setParam($key, $value);
-            }
-        }
-
-        private function setParam($statment, $key, $value){
-
-            $statment->bindParam($key, $value);
-        }
-
-        public function query($RawQuery, $params = array()){
-
-            $stmt = $this->conn->prepare($RawQuery);
-
-            $this->setParams($stmt, $params);
-
-            return $stmt->execute();
-           
+            $this->setParam($statment, $key, $value);
         }
     }
 
-?>
+    private function setParam($statment, $key, $value)
+    {
+
+        $statment->bindParam($key, $value);
+    }
+
+    public function execQuery($rawQuery, $params = array())
+    {
+
+        $stmt = $this->conn->prepare($rawQuery);
+
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function select($rawQuery, $params = array()): array
+    {
+
+        $stmt = $this->execQuery($rawQuery, $params);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
